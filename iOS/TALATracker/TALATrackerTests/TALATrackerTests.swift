82f577163c3864e3a993e4ae7fd99fa2ca02f811
Frederik Lohner <freddylohner@gmail.com>
2015-10-04 14:57:37 -0600

    Created base iOS skeleton

---
+++ iOS/TALATracker/TALATrackerTests/TALATrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  TALATrackerTests.swift
+//  TALATrackerTests
+//
+//  Created by Frederik Lohner on 4/Oct/15.
+//  Copyright © 2015 CSCI3308. All rights reserved.
+//
+
+import XCTest
+@testable import TALATracker
+
+class TALATrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
